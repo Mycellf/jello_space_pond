df59cf33eeb4a19355ca3079f14c52aa1d1bc539
jeffrey <jcouch210@gmail.com>
2025-07-30 12:31:04 -0800

    Setup macroquad boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::{
+    input::{self, KeyCode},
+    window::{self, Conf},
+};
+
+const START_IN_FULLSCREEN: bool = true;
+
+fn config() -> Conf {
+    Conf {
+        window_title: "Jello Space Pond".to_owned(),
+        fullscreen: START_IN_FULLSCREEN,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(config)]
+async fn main() {
+    let mut fullscreen = START_IN_FULLSCREEN;
+
+    loop {
+        if input::is_key_pressed(KeyCode::F11) {
+            fullscreen ^= true;
+            macroquad::window::set_fullscreen(fullscreen);
+        }
+
+        window::next_frame().await;
+    }
 }
